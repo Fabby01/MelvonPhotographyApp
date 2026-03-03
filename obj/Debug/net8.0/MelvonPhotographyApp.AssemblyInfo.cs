cdd553baa7c7a6a62b585aba722020ad2e49c672
Fabby01 <fabimko036@student.polsl.pl>
2026-03-03 16:22:01 +0100

    Fix: Remove duplicate Scripts section in Booking page

--- obj/Debug/net8.0/MelvonPhotographyApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MelvonPhotographyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MRMstudios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f69eb75e6ec58925e31cea51c92668f3b1a50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+450a425f71587fb90eee7c22f33549f614dd05db")]
 [assembly: System.Reflection.AssemblyProductAttribute("MRMstudios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MRMstudios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
